04f856abb3cbe456a2a22d277d9955c9c9476037
Zahid11111 <110874397+Zahid11111@users.noreply.github.com>
2025-01-14 01:24:04 +0400

    sd

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+# app.py
+
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+# Load the trained model
+model_path = 'model.pkl'
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Extract data from form
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    
+    # Make prediction
+    prediction = model.predict(final_features)
+    output = 'Placed' if prediction[0] == 1 else 'Not Placed'
+
+    return render_template('index.html', prediction_text='Prediction: {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
