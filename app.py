2d94faccc24cdf25014c9771b01172524c37c947
Zahid11111 <110874397+Zahid11111@users.noreply.github.com>
2025-01-14 02:27:58 +0400

    asd

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ def predict():
         return jsonify({'error': 'An error occurred while processing your request.'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT from environment or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
